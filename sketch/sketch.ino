f028e3bc990ee7505870ee03faf9935c25066554
Shailey Joseph <shaileyjoseph2006@gmail.com>
2025-12-05 11:17:21 -0800

    renamed code folder to comply with arduino ide

---
+++ sketch/sketch.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Servo wheelServo;
+  Servo armServo;
+  Servo elbowServo;
+  Servo clawServo;
+
+  wheelServo.attach(...);//replace ... with appropriate pin number on arduino
+  armServo.attach(...);
+  elbowServo.attach(...);
+  clawServo.attach(...);
+}
+
+void pick_up_rock()
+{
+
+}
+
+void rotate_rover()
+{
+
+}
+
+void move()
+{
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
